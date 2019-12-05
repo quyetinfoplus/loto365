95019e1c52706a3f37dd14ab6bcef4444d38a5ab
quyet@infoplusvn.com <quyet@infoplusvn.com>
2019-12-05 11:29:19 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/tabs/tabs/tabs.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+})
+export class TabsPage implements OnInit {
+  colorKQ = '#FFFFFF';
+  colorTD = '#FFFFFF' ;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
