445dd88d6a2280edfd1711cf82645bd86ff843c4
quyet@infoplusvn.com <quyet@infoplusvn.com>
2019-12-31 17:50:38 +0700

    31122019 ket qua mien nam and mien trung

--- src/app/service/env.service.ts
+++ src/app/service/env.service.ts
@@ -6,7 +6,9 @@ import { Injectable } from '@angular/core';
 export class EnvService {
   API_URL = 'http://1.55.215.217:8081/';
   URL_LOAD_DATA_THEO_DOI = 'kqxs/chotkq';
-  URL_LOAD_DATA_KET_QUA = 'kqxs/ketqua';
+  URL_LOAD_DATA_KET_QUA_MB = 'kqxs/ketqua';
+  URL_LOAD_DATA_KET_QUA_MN = 'kqxs/ketquamn';
+  URL_LOAD_DATA_KET_QUA_MT = 'kqxs/ketquamt';
   URL_LOAD_DATA_TRENDING = 'kqxs/trending';
   URL_LOAD_DATA_PHAN_TICH = 'kqxs/caudep';
   URL_GET_MAX_DO_DAI_CAU = 'kqxs/limitday';
