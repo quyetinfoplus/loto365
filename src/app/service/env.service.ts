d662ef246c0ea1a7006ae06d7b9b390b044215f0
quyet@infoplusvn.com <quyet@infoplusvn.com>
2019-12-31 18:01:07 +0700

    update

--- src/app/service/env.service.ts
+++ src/app/service/env.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class EnvService {
   API_URL = 'http://1.55.215.217:8081/';
   URL_LOAD_DATA_THEO_DOI = 'kqxs/chotkq';
+  URL_LOAD_DATA_KET_QUA = 'kqxs/ketqua';
   URL_LOAD_DATA_KET_QUA_MB = 'kqxs/ketqua';
   URL_LOAD_DATA_KET_QUA_MN = 'kqxs/ketquamn';
   URL_LOAD_DATA_KET_QUA_MT = 'kqxs/ketquamt';
